8c74d202905c2a44356fce441d5c5da3f9e08e80
Danel Boikis <danelbo@post.bgu.ac.il>
2024-05-16 19:01:56 +0300

    fix

--- backend/business/__init__.py
+++
@@ -1,7 +0,0 @@
-from currency_converter import CurrencyConverter
-
-# Initialize CurrencyConverter object
-c = CurrencyConverter()
-
-# Import the User class so it's accessible from the package
-from .user import User
