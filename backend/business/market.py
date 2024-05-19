910df87a59f4cc65910c80f4ece80248e967f6c6
michaeli123 <adarmic@post.bgu.ac.il>
2024-05-19 12:59:54 +0300

    done with integration of tps

--- backend/business/market.py
+++ backend/business/market.py
@@ -1,6 +1,7 @@
 from user.user import UserFacade, NotificationDTO
 from store.store import StoreFacade
-from ThirdPartyHandlers.third_party_handlers import PaymentHandler
+from ThirdPartyHandlers.third_party_handlers import PaymentHandler, SupplyHandler
+from notifier import Notifier
 from typing import Optional, List, Dict
 import threading
 
@@ -41,7 +42,7 @@ class MarketFacade:
             # here you can add fields
             self.user_facade = UserFacade()
             self.store_facade = StoreFacade()
-            self.payment_handler = PaymentHandler()
+
 
     def show_notifications(self, user_id: int) -> List[NotificationDTO]:
         return self.user_facade.get_notifications(user_id)
@@ -51,33 +52,37 @@ class MarketFacade:
             if self.store_facade.check_product_availability(store_id, product_id, amount):
                 self.user_facade.add_product_to_basket(user_id, store_id, product_id, amount)
 
-    def checkout(self, user_id: int, payment_details: Dict):
-        basket = self.user_facade.get_shopping_cart(user_id)
+    def checkout(self, user_id: int, payment_details: Dict, address: Dict):
+        cart = self.user_facade.get_shopping_cart(user_id)
         # lock the __lock
         with MarketFacade.__lock:
 
             # check if the products are still available
-            for store_id, products in basket.items():
+            for store_id, products in cart.items():
                 for product_id, amount in products.items():
                     if not self.store_facade.check_product_availability(store_id, product_id, amount):
                         raise ValueError(f"Product {product_id} is not available in the required amount")
 
             # charge the user
-            amount = self.store_facade.calculate_total_price(basket)
-            if not self.payment_handler.process_payment(amount, payment_details):
+            amount = self.store_facade.calculate_total_price(cart)
+            if not PaymentHandler().process_payment(amount, payment_details):
                 raise ValueError("Payment failed")
 
             # remove the products from the store
-            for store_id, products in basket.items():
+            for store_id, products in cart.items():
                 for product_id, amount in products.items():
                     self.store_facade.remove_product(store_id, product_id, amount)
 
-        # clear the basket
+        # clear the cart
         self.user_facade.clear_basket(user_id)
 
         # TODO: create a purchase
-        # TODO: deliver the products
-        # TODO: notify (?)
+
+        package_details = {'shopping cart': cart, 'address': address}
+        if not SupplyHandler().process_supply(package_details, user_id):
+            raise ValueError("Supply failed")
+        for store_id in cart.keys():
+            Notifier().notify_new_purchase(store_id, user_id)
 
     def add_store_owner(self, store_id: int, owner_id: int, new_owner_id: int):
         # TODO:
