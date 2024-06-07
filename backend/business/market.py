d62344bed515e2c0929c94c956e7a6671851410b
ilayec <ilayevenchen@gmail.com>
2024-06-07 18:46:09 +0300

    done

--- backend/business/market.py
+++ backend/business/market.py
@@ -162,7 +162,7 @@ class MarketFacade:
 
     def nominate_store_owner(self, store_id: int, owner_id: int, new_owner_username):
         # get user_id of new_owner_username
-        new_owner_id = self.user_facade.get_usernames()[new_owner_username]
+        new_owner_id = self.user_facade.get_user_id_from_username(new_owner_username)
         nomination_id = self.roles_facade.nominate_owner(store_id, owner_id, new_owner_id)
         # TODO: different implementation later
         self.user_facade.notify_user(new_owner_id,
