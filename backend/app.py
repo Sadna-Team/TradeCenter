e072d1a8f1dd136732a2a608f204b63f45b38a11
ilayec <ilayevenchen@gmail.com>
2024-06-11 18:34:15 +0300

    added a socket to the flask app

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_socketio import SocketIO
 # from service_layer.routes import api_routes  # Import API routes from service_layer
 from __init__ import create_app
 import logging
 
 logger = logging.getLogger('myapp')
 
-
 app = create_app()
+socketio = SocketIO(app)
+
 # Register API routes from service_layer
 #app.register_blueprint(api_routes)
 
