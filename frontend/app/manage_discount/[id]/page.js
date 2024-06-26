3addab41d4b7147f7454c4c954854ca7a0715648
shoval nachum <nashov@post.bgu.ac.il>
2024-06-27 02:35:22 +0300

    Sends store_id to the management pages

--- frontend/app/manage_discount/[id]/page.js
+++ frontend/app/manage_discount/[id]/page.js
@@ -1,8 +1,14 @@
+"use client";
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 const ManageEmployee = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
 
     return (
         <div>
-          <h1>manage_employee</h1>
+          <h1>manage_employee {id}</h1>
         </div>
       );
 }
--- frontend/app/manage_policy/[id]/page.js
+++ frontend/app/manage_policy/[id]/page.js
@@ -1,8 +1,15 @@
+"use client";
+import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 const ManagePolicy = () => {
+
+  const searchParams = useSearchParams();
+  const id = searchParams.get('id');
 
     return (
         <div>
-          <h1>manage_policy</h1>
+          <h1>manage_policy {id}</h1>
         </div>
       );
 }
