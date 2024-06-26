56150599b781c69274040984b513dd16a3a46a77
shoval nachum <nashov@post.bgu.ac.il>
2024-06-26 18:56:05 +0300

    Edited the skeleton of the store

---
+++ frontend/app/manage_discount/page.js
@@ -0,0 +1,10 @@
+const ManageEmployee = () => {
+
+    return (
+        <div>
+          <h1>manage_employee</h1>
+        </div>
+      );
+}
+
+export default ManageEmployee;
---
+++ frontend/app/manage_employee/page.js
@@ -0,0 +1,9 @@
+const manage_employee = () => {
+    return (
+        <div>
+          <h1>manage_employee</h1>
+        </div>
+      );
+}
+
+export default manage_employee;
