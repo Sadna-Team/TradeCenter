3165ddba35d502fcbb817d2714abc1ff12174abd
shoval nachum <nashov@post.bgu.ac.il>
2024-06-26 15:48:37 +0300

    Removed unused test.py file and update Button component styling

--- frontend/components/Button.jsx
+++ frontend/components/Button.jsx
@@ -1,8 +1,10 @@
 export default function Button({ children, onClick, className }) {
-    return (
-      <button onClick={onClick} className={`bg-blue-500 text-white py-2 px-4 rounded ${className}`}>
-        {children}
-      </button>
-    );
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-[#CB4335] text-white py-2 px-4 rounded ${className}`}
+    >
+      {children}
+    </button>
+  );
 }
-  
