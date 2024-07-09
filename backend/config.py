bb9e78d91955bb0f4676ff7bd83243ad1e1f491d
Danel Boikis <danelbo@post.bgu.ac.il>
2024-07-09 21:49:29 +0300

    fixed up issues with running the app outside of the container

--- backend/config.py
+++ backend/config.py
@@ -15,16 +15,26 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL')
 
+class DockerDevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DOCKER_DEV_DATABASE_URL')
+
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
 
+class DockerTestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DOCKER_TEST_DATABASE_URL')
+
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DOCKER_DATABASE_URL')
 
 config = {
     'development': DevelopmentConfig,
+    'docker_development': DockerDevelopmentConfig,
     'testing': TestingConfig,
+    'docker_testing': DockerTestingConfig,
     'production': ProductionConfig,
     'default': DevelopmentConfig
 }
