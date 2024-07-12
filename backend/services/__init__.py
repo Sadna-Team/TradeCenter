1406db3edbf5599c048a947ad28d6bc11d0e4796
Gal <Gala0@post.bgu.ac.il>
2024-07-12 12:46:10 +0300

    Almost done

---
+++ backend/services/__init__.py
@@ -0,0 +1,102 @@
+from .ecommerce_services.controllers import PurchaseService
+from .store_services.controllers import StoreService
+from .user_services.controllers import UserService, AuthenticationService
+
+
+class initial_state:
+    def __init__(self):
+        self.purchase_service = PurchaseService()
+        self.store_service = StoreService()
+        self.user_service = UserService()
+        self.authentication_service = AuthenticationService()
+
+        self.file = None
+        self.username_to_token = {}
+        self.username_to_password = {}
+        self.store_name_to_id = {}
+
+    def create_user(self, username, password, email, phone, year, month, day):
+        register_credentials = {
+            'password': password,
+            'email': email,
+            'username': username,
+            'year': year,
+            'month': month,
+            'day': day,
+            'phone': phone
+        }
+
+        response = self.authentication_service.start_guest()
+        if response.status_code != 200:
+            return False
+        token = response.json()['token']
+
+        response = self.authentication_service.register(token, register_credentials)
+        if response.status_code != 201:
+            return False
+
+        self.username_to_password[username] = password
+        return True
+
+    def login_user(self, username):
+        token = self.username_to_token.get(username)
+        password = self.username_to_password.get(username)
+        if token is not None and password is not None:
+            response = self.authentication_service.login(username, password)
+            if response.status_code != 200:
+                return False
+            token = response.json()['token']
+            self.username_to_token[username] = token
+
+    def add_system_manager(self, username):
+        response = self.user_service.add_system_manager(0, username)
+        if response.status_code != 200:
+            return False
+        return True
+
+    def add_store(self, username, store_name):
+        token = self.username_to_token.get(username, None)
+        if token is None:
+            return False
+
+        response = self.authentication_service.get_user_id(token)
+        if response.status_code != 200:
+            return False
+        user_id = response.json()['user_id']
+
+        response = self.store_service.add_new_store(user_id, 'address', 'city', 'state', 'country', '12345', store_name)
+        if response.status_code != 200:
+            return False
+        return True
+
+    def add_product(self, username, store_name, product_name, price, quantity):
+        token = self.username_to_token.get(username, None)
+        if token is None:
+            return False
+
+        response = self.authentication_service.get_user_id(token)
+        if response.status_code != 200:
+            return False
+        user_id = response.json()['user_id']
+
+        response = self.store_service.get_store_id(store_name)
+        if response.status_code != 200:
+            return False
+        store_id = response.json()['store_id']
+
+        # def add_product_to_store(self, user_id: int, store_id: int, product_name: str, description: str, price: float,
+        response = self.store_service.add_product_to_store(user_id, store_id, product_name, 'description', price, 1, [],
+                                                           quantity)
+        if response.status_code != 200:
+            return False
+        return True
+
+    def add_owner(self, username_actor, username, store_name):
+        pass
+
+    def add_manager(self, username_actor, username, store_name, permissions):
+        pass
+
+    def logout(self, username):
+        pass
+
