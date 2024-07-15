1b2e5c4064b3669fffb2c803f6a114b5e547cf5a
ilayec <ilayevenchen@gmail.com>
2024-07-15 22:10:00 +0300

    now moving to testing

--- backend/business/user/user.py
+++ backend/business/user/user.py
@@ -10,7 +10,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, T
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, backref, sessionmaker
 import json
-import threading
+from ...database import db
 
 import logging
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w',
@@ -45,6 +45,8 @@ class ShoppingBasket(Base):
         products[product_id] = products.get(product_id, 0) + quantity
         self.products = json.dumps(products)
 
+        db.session.commit()
+
     def get_dto(self) -> Dict[int, int]:
         return json.loads(self.products)
 
@@ -59,6 +61,8 @@ class ShoppingBasket(Base):
             del products[product_id]
         self.products = json.dumps(products)
 
+        db.session.commit()
+
     def subtract_product(self, product_id: int, quantity: int):
         products = json.loads(self.products)
         if product_id not in products:
@@ -68,6 +72,7 @@ class ShoppingBasket(Base):
         products[product_id] -= quantity
         self.products = json.dumps(products)
 
+        db.session.commit()
 
 
 # Didn't do logs for this class
@@ -85,7 +90,10 @@ class ShoppingCart(Base):
         if not basket:
             basket = ShoppingBasket(store_id, self.id)
             self.shopping_baskets.append(basket)
+            db.session.add(basket)
         basket.add_product(product_id, quantity)
+
+        db.session.commit()
 
     def get_dto(self) -> Dict[int, Dict[int, int]]:
         return {basket.store_id: basket.get_dto() for basket in self.shopping_baskets}
@@ -96,11 +104,15 @@ class ShoppingCart(Base):
             raise StoreError("Store not found", StoreErrorTypes.store_not_found)
         basket.remove_product(product_id, quantity)
 
+        db.session.commit()
+
     def subtract_product_from_cart(self, store_id: int, product_id: int, quantity: int) -> None:
         basket = next((b for b in self.shopping_baskets if b.store_id == store_id), None)
         if not basket:
             raise StoreError("Store not found", StoreErrorTypes.store_not_found)
         basket.subtract_product(product_id, quantity)
+
+        db.session.commit()
 
 
 class Notification(Base):
@@ -114,6 +126,9 @@ class Notification(Base):
     def __init__(self, message: str, date: datetime) -> None:
         self.message = message
         self.date = date
+
+    def get_notification_dto(self) -> NotificationDTO:
+        return NotificationDTO(self.id, self.message, self.date)
 
 
 class State(ABC):
@@ -213,6 +228,7 @@ class Member(Base):
     def set_suspense(self, value: bool, suspended_until: Optional[datetime]):
         self.is_suspended = value
         self.suspended_until = suspended_until
+        db.session.commit()
 
     def get_password(self):
         return self.password
@@ -222,9 +238,11 @@ class Member(Base):
 
     def add_notification(self, notification: Notification):
         self.notifications.append(notification)
+        db.session.commit()
 
     def clear_notifications(self):
         self.notifications.clear()
+        db.session.commit()
 
 
 class User(Base):
@@ -245,18 +263,29 @@ class User(Base):
         self.member = None
         self.shopping_cart = ShoppingCart(self.id)
 
+    def is_member(self):
+        return self.member is not None
+    
     def add_notification(self, notification: Notification) -> None:
-        if self.member:
+        if self.is_member():
             self.member.add_notification(notification)
 
+        db.session.add(notification)
+        db.session.commit()
+
     def get_notifications(self) -> List[Notification]:
-        if self.member:
+        if self.is_member():
             return self.member.get_notifications()
         return []
 
     def clear_notifications(self) -> None:
-        if self.member:
+        if self.is_member():
             self.member.clear_notifications()
+        
+        notifications = Notification.query.filter_by(member_id=self.member.id).all()
+        for notification in notifications:
+            db.session.delete(notification)
+        db.session.commit()
 
     def add_product_to_basket(self, store_id: int, product_id: int, quantity: int) -> None:
         self.shopping_cart.add_product_to_basket(store_id, product_id, quantity)
@@ -265,9 +294,11 @@ class User(Base):
         return self.shopping_cart.get_dto()
 
     def register(self, email: str, username: str, password: str, year: int, month: int, day: int, phone: str) -> None:
-        if self.member:
+        if self.is_member():
             raise UserError("User is already registered", UserErrorTypes.user_already_registered)
         self.member = Member(email, username, password, year, month, day, phone)
+        db.session.add(self.member)
+        db.session.commit()
 
     def remove_product_from_basket(self, store_id: int, product_id: int, quantity: int):
         self.shopping_cart.remove_product_from_basket(store_id, product_id, quantity)
@@ -277,26 +308,25 @@ class User(Base):
 
     def clear_basket(self):
         self.shopping_cart = ShoppingCart(self.id)
+        db.session.commit()
 
     def get_password(self):
-        if self.member:
+        if self.is_member():
             return self.member.get_password()
         raise UserError("User is not registered", UserErrorTypes.user_not_registered)
 
-    def is_member(self):
-        return self.member is not None
-    
     def is_suspended(self):
-        if self.member:
+        if self.is_member():
             return self.member.is_suspended
         return False
     
     def change_suspend(self, value: bool, suspended_until: Optional[datetime]):
-        if self.member:
+        if self.is_member():
             self.member.set_suspense(value, suspended_until)
+        raise UserError("User is not registered", UserErrorTypes.user_not_registered)
 
     def create_purchase_user_dto(self) -> PurchaseUserDTO:
-        if self.member:
+        if self.is_member():
             return PurchaseUserDTO(self.id, self.member.birthdate)
         return PurchaseUserDTO(self.id)
 
@@ -312,6 +342,8 @@ class User(Base):
         for store_id, products in cart.items():
             for product_id, quantity in products.items():
                 self.add_product_to_basket(store_id, product_id, quantity)
+        
+        db.session.commit()
 
 
 class UserFacade:
@@ -319,8 +351,8 @@ class UserFacade:
     __create_lock = threading.Lock()
     __register_lock = threading.Lock()
     __notification_lock = threading.Lock()
+    __suspend_lock = threading.Lock()
     _instance = None
-    __id_serializer: int = 0
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
@@ -330,21 +362,21 @@ class UserFacade:
     def __init__(self):
         if not hasattr(self, '_initialized'):
             self._initialized = True
-            self.__users: Dict[int, User] = {}
-            self.__usernames: Dict[str, int] = {}  # username -> user_id
-            self.__suspend_users: Dict[int, Optional[datetime]] = {} # user_id's -> date until end of suspension
 
     def clean_data(self):
         """
         For testing purposes only
         """
-        self.__users.clear()
-        self.__usernames.clear()
-        UserFacade.__id_serializer = 0
-        self.__suspend_users.clear()
+        db.session.query(User).delete()
+        db.session.commit()
 
     def get_suspended_users(self) -> Dict[int, Optional[datetime]]:
-        return self.__suspend_users
+        # suspended_users = SuspendedUser.query.all()
+        # return {su.user_id: su.suspension_end for su in suspended_users}
+        users = User.query.all()
+        with UserFacade.__suspend_lock:
+            out = {user.id: user.member.suspended_until for user in users if user.is_suspended()}
+        return out
 
     def suspended(self, user_id: int) -> bool:
         """
@@ -352,17 +384,23 @@ class UserFacade:
         otherwise we check if the user is registered and suspended
         * Return True if we can't continue
         """
-        if not self.__get_user(user_id).is_member():
-            return False
-        return self.__get_user(user_id).is_suspended()
+        user = User.query.filter_by(id=user_id).first()
+        with UserFacade.__suspend_lock:
+            if not user:
+                return False
+            ans = self.user.is_suspended()
+        return ans
 
     def suspend_user_permanently(self, user_id: int):
         """
         Suspend user permanently, only system manager can do this
         * user_id: the id of the user to suspend
         """
-        self.__get_user(user_id).change_suspend(True, None)
-        self.__suspend_users[user_id] = None # Added the user to the suspended users list
+        user = User.query.filter_by(id=user_id).first()
+        with UserFacade.__suspend_lock:
+            if not user:
+                raise UserError("User not found", UserErrorTypes.user_not_found)
+            user.change_suspend(True, None)
 
     def suspend_user_temporarily(self, user_id: int, date_details: dict, time_details: dict):
         """
@@ -372,8 +410,12 @@ class UserFacade:
         * time_details: the time until the user is suspended (hour, minute)
         """
         date = datetime(int(date_details["year"]), int(date_details["month"]), int(date_details["day"]), int(time_details["hour"]), int(time_details["minute"]))    
-        self.__get_user(user_id).change_suspend(True, date)
-        self.__suspend_users[user_id] = date # Added the user to the suspended users list
+
+        user = User.query.filter_by(id=user_id).first()
+        with UserFacade.__suspend_lock:
+            if not user:
+                raise UserError("User not found", UserErrorTypes.user_not_found)
+            user.change_suspend(True, date)
 
     def unsuspend_user(self, user_id: int):
         """
@@ -381,42 +423,48 @@ class UserFacade:
         * actor_id: the id of system manager
         * user_id: the id of the user to unsuspend
         """
-        self.__get_user(user_id).change_suspend(False, None)
-        self.__suspend_users.pop(user_id) # Removed the user from the suspended users list
+        user = User.query.filter_by(id=user_id).first()
+        with UserFacade.__suspend_lock:
+            if not user:
+                raise UserError("User not found", UserErrorTypes.user_not_found)
+            user.change_suspend(False, None)
 
     def __get_user(self, user_id: int) -> User:
-        if user_id not in self.__users:
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
             raise UserError("User not found", UserErrorTypes.user_not_found)
-        return self.__users[user_id]
+        return user
 
     def get_userDTO(self, user_id: int, role: str = None) -> UserDTO:
         user = self.__get_user(user_id)
         return user.get_user_dto(role)
 
     def create_user(self, currency: str = "USD") -> int:
-        with UserFacade.__create_lock:
-            new_id = UserFacade.__id_serializer
-            UserFacade.__id_serializer += 1
         user = User(currency)
-        self.__users[new_id] = user
-        return new_id
+        db.session.add(user)
+        db.session.commit()
+        return user.id
     
-    def register_user(self, user_id: int, email: str, username: str, password: str,
+    def register_user(self, user_id, email: str, username: str, password: str,
                       year: int, month: int, day: int, phone: str) -> None:
         with UserFacade.__register_lock:
-            if username in self.__usernames:
-                raise UserError("Username already exists", UserErrorTypes.username_already_exists)
-            if user_id not in self.__users:
+            user = User.query.filter_by(id=user_id).first()
+            if not user:
                 raise UserError("User not found", UserErrorTypes.user_not_found)
-            # "False" is for is_suspended field
-            self.__get_user(user_id).register(email, username, password, year, month, day, phone)
-            self.__usernames[username] = user_id
+            if user.is_member():
+                raise UserError("User is already registered", UserErrorTypes.user_already_registered)
+            user = User.query.filter_by(username=username).first()
+            if user:
+                raise UserError("Username already exists", UserErrorTypes.username_already_exists)
+            
+            user.register(email, username, password, year, month, day, phone)
 
     def get_user_id_from_username(self, username: str) -> int:
-        if username in self.__usernames.keys():
-            return self.__usernames[username]
-        raise UserError("Username not found", UserErrorTypes.username_not_found)
-    
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            raise UserError("Username not found", UserErrorTypes.username_not_found)
+        return user.id
+
     def get_notifications(self, user_id: int) -> List[NotificationDTO]:
         with UserFacade.__notification_lock:
             notifications = self.__get_user(user_id).get_notifications()
@@ -427,9 +475,10 @@ class UserFacade:
         return out
 
     def get_userid(self, username: str) -> int:
-        if username not in self.__usernames:
-            raise UserError("User not found", UserErrorTypes.user_not_found)
-        return self.__usernames[username]
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            raise UserError("Username not found", UserErrorTypes.username_not_found)
+        return user.id
 
     def clear_notifications(self, user_id: int) -> None:
         with UserFacade.__notification_lock:
@@ -438,44 +487,44 @@ class UserFacade:
     def notify_user(self, user_id: int, notification: NotificationDTO) -> None:
         with UserFacade.__notification_lock:
             self.__get_user(user_id).add_notification(
-                Notification(notification.get_notification_id(),
-                             notification.get_message(), notification.get_date()))
+                Notification(notification.get_message(), notification.get_date()))
 
     def add_product_to_basket(self, user_id: int, store_id: int, product_id: int, quantity: int) -> None:
-        if self.suspended(user_id):
-            raise UserError("User is suspended", UserErrorTypes.user_suspended)
+        with UserFacade.__suspend_lock:
+            if self.suspended(user_id):
+                raise UserError("User is suspended", UserErrorTypes.user_suspended)
         self.__get_user(user_id).add_product_to_basket(store_id, product_id, quantity)
 
     def get_shopping_cart(self, user_id: int) -> Dict[int, Dict[int, int]]:
-        if self.suspended(user_id):
-            raise UserError("User is suspended", UserErrorTypes.user_suspended)
+        with UserFacade.__suspend_lock:
+            if self.suspended(user_id):
+                raise UserError("User is suspended", UserErrorTypes.user_suspended)
         return self.__get_user(user_id).get_shopping_cart()
 
     def remove_product_from_basket(self, user_id: int, store_id: int, product_id: int, quantity: int) -> None:
-        if self.suspended(user_id):
-            raise UserError("User is suspended", UserErrorTypes.user_suspended)
+        with UserFacade.__suspend_lock:
+            if self.suspended(user_id):
+                raise UserError("User is suspended", UserErrorTypes.user_suspended)
         self.__get_user(user_id).remove_product_from_basket(store_id, product_id, quantity)
 
     def clear_basket(self, user_id: int) -> None:
-        if self.suspended(user_id):
-            raise UserError("User is suspended", UserErrorTypes.user_suspended)
+        with UserFacade.__suspend_lock:
+            if self.suspended(user_id):
+                raise UserError("User is suspended", UserErrorTypes.user_suspended)
         self.__get_user(user_id).clear_basket()
 
     def get_password(self, username: str) -> Tuple[int, str]:
-        if username not in self.__usernames:
-            raise UserError("User not found", UserErrorTypes.user_not_found)
-        user_id = self.__usernames.get(username)
-        return user_id, self.__get_user(user_id).get_password()
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            raise UserError("Username not found", UserErrorTypes.username_not_found)
+        return user.id, user.get_password()
 
     def remove_user(self, user_id: int):
-        if user_id in self.__users:
-            del self.__users[user_id]
-        else:
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
             raise UserError("User not found", UserErrorTypes.user_not_found)
-        for username, u_id in self.__usernames.items():
-            if u_id == user_id:
-                del self.__usernames[username]
-                break
+        db.session.delete(user)
+        db.session.commit()
 
     def is_member(self, user_id: int) -> bool:
         return self.__get_user(user_id).is_member()
@@ -497,9 +546,9 @@ class UserFacade:
         user.set_cart(cart)
 
     def get_all_members(self) -> List[UserDTO]:
+        users = User.query.all()
         out = []
-        for user_id, user in self.__users.items():
+        for user in users:
             if user.is_member():
                 out.append(user.get_user_dto())
         return out
-
